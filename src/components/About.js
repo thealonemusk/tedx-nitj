9de04ea720e56a5386a05429542071ee321cbdd4
Nishchay <nishchayveer19@gmail.com>
2023-03-12 14:03:39 +0530

    background changed

--- src/components/About.js
+++ src/components/About.js
@@ -8,30 +8,32 @@ export const About = () => {
 
     // useEffect(() => {
     //     const el = aboutTed.current;
-        
+
     //     gsap.fromTo(el, {opacity : 0}, {opacity : 1, duration: 4, scrollTrigger : {
     //         trigger: el,
-            
+
     //     }})
-       
+
     // },[])
     return (
         <div className="about__page">
-            {/* <div class='rotate'>
-                <h1><span className="what">WHAT</span>IS</h1>
-            </div> */}
+
             <div className="about">
-                <div className='row about__blocks'> 
-                    <div className='col-lg-4 col-md-6 about__ted'>
+
+                <div className='row about__blocks'>
+                    <div className='col-lg-3 col-md-6 about__ted align-self-center'>
+                        <h1 className='vertical-text'><span className="what">WHAT</span>IS</h1>
+                    </div>
+                    <div className='col-lg-3 col-md-6 about__ted'>
                         <h1 className='about_heading'>TED</h1>
                         <p>TED is a nonpartisan nonprofit devoted to spreading ideas, usually in the form of short, powerful talks. TED began in 1984 as a conference where Technology, Entertainment and Design converged, and today covers almost all topics — from science to business to global issues — in more than 110 languages. Meanwhile, independently run TEDx events help share ideas in communities around the world.</p>
                     </div>
-                    <div className='col-lg-4 col-md-6 about__ted'>
+                    <div className='col-lg-3 col-md-6 about__ted'>
                         <h1 className='about_heading'>TEDx</h1>
                         <p>A TEDx event is a local gathering where live TED-like talks and performances are shared with the community. TEDx events are fully planned and coordinated independently, on a community-by-community basis. The content and design of each TEDx event is unique and developed independently, but all of them have features in common.</p>
                     </div>
-                    <div className='col-lg-4 col-md-12 about__ted'>
-                        <h1 className='about_heading'>TEDx<span className="clg-name">NITJalandhar</span></h1>
+                    <div className='col-lg-3 col-md-12 about__ted'>
+                        <h1 className='about_heading'>TEDx<span className="clg-name">NITJ</span></h1>
                         <p>We started off as a group of driven people to launch the TEDx community at our institute in October 2022. Today, we're a family of about 50 people working to provide a stage for ideas and stories from different fields to reach keen and curious minds. TEDxNITJalandhar is organised by a team of dynamic individuals who believe in the power of reverberation of ideas.</p>
                     </div>
 
