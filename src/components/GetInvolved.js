8df3748a1301d456d0104d99243b34051b4641f9
Nishchay <nishchayveer19@gmail.com>
2023-03-17 22:48:14 +0530

    get involve

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import React from 'react'
+import { FolderMinus, GraphUpArrow, Link, Person, PersonCheckFill, Question, SearchHeartFill, Thunderbolt } from 'react-bootstrap-icons';
 import './getinvolve.css';
 export const GetInvolved = () => {
   return (
@@ -9,28 +12,28 @@ export const GetInvolved = () => {
                 </div>
                 <div class="row">
                     <div class="col-md-3 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-question"></i></div>
+                        <div class="sym"><div class="fa"><Question/></div></div>
                         <div class="symdes">
                             <h4 class="whitefont">Learn</h4>
                             <p class="whitefont">Do your research by reading literally everything on this web page.</p>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-wpforms"></i></div>
+                        <div class="sym"><div class="fa"><FolderMinus/></div></div>
                         <div class="symdes">
                             <h4 class="whitefont">Apply</h4>
                             <p class="whitefont">Fill out an application.</p>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-user-plus"></i></div>
+                        <div class="sym"><div class="fa"><Person/></div></div>
                         <div class="symdes">
                             <h4 class="whitefont">Review &amp; Selection</h4>
                             <p class="whitefont">Our speaker selection committee will review your proposal and contact you.</p>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-street-view"></i></div>
+                        <div class="sym"><div class="fa"><PersonCheckFill/></div></div>
                         <div class="symdes">
                             <h4 class="whitefont">Curation</h4>
                             <p class="whitefont">If selected, we'll help you perfect your presentation for a TEDx audience.</p>
@@ -50,21 +53,21 @@ export const GetInvolved = () => {
                 </div>
                 <div class="row">
                     <div class="col-md-4 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-link"></i></div>
+                        <div class="sym"><div class="fa"><Link/></div></div>
                         <div class="symdes">
                             <h4>Brand Linking</h4>
                             <p>Associate with a powerful worldwide brand and movement synonymous with creativity, collaboration, and innovation.</p>
                         </div>
                     </div>
                     <div class="col-md-4 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-line-chart"></i></div>
+                        <div class="sym"><div class="fa"><GraphUpArrow/></div></div>
                         <div class="symdes">
                             <h4>Lead Generation</h4>
                             <p>Gain highly qualified leads from tuned-in intellectuals across a variety of industries, including education, healthcare, and technology.</p>
                         </div>
                     </div>
                     <div class="col-md-4 col-sm-12 text-center">
-                        <div class="sym"><i class="fa fa-bolt"></i></div>
+                        <div class="sym"><div class="fa"><Thunderbolt/></div></div>
                         <div class="symdes">
                             <h4>Unconventional</h4>
                             <p>Access unconventional showcase opportunities unique to our conference, creating genuine experiences that drive higher engagement.</p>
