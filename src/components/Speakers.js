13515507e99bb0c99dbba35ba36d6f32d2ac871a
Nishchay <nishchayveer19@gmail.com>
2023-03-28 03:03:43 +0530

    speaker using api

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Team.css'
 import SpeakerCard from './SpeakerCard'
+import { useState } from 'react'
 export const Speakers = () => {
+  const [speakers, setSpeakers] = useState([{}])
+  useEffect(() => {
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    // };
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://tedx.cyclic.app/api/speakers/get", requestOptions)
+      .then(response => response.json())
+      .then(result => setSpeakers(result))
+      .catch(error => console.log('error', error));
+
+  }, [])
+  // let speakerObject = JSON.parse(speakers)
   return (
     <div className='speakers'>
 
@@ -10,14 +30,23 @@ export const Speakers = () => {
           <h3>SPEAKERS</h3>
         </div>
         <div class="SpeakerCarousel__Carousel-sc-tve874-2 fRhUJv">
-        <SpeakerCard name="Sandeep Sarma" position="Head of Design and Content, Xiaomi India" source="https://media.licdn.com/dms/image/D5603AQHCdWcFndwU8g/profile-displayphoto-shrink_800_800/0/1671101820207?e=2147483647&v=beta&t=Vv4_gXhKOm2KA7SLbEA2JT_DWEWr121bu3DnioZ4ZxY" description="Sandeep Sarma is a prominent figure in the world of design and content, currently serving as the Head of Design and Content at Xiaomi. With a passion for user experience and a keen eye for detail, his work has helped shape the company's brand image and product offerings. His journey in the industry has been nothing short of remarkable, and his impact continues to reverberate across the tech landscape." insta='https://instagram.com/sandeepsarma?igshid=YmMyMTA2M2Y' />
+          {speakers && speakers.map(speaker => (
+            <SpeakerCard
+              name={speaker.name}
+              position={speaker.title}
+              source={speaker.imageURL}
+              description={speaker.description}
+              twitter={speaker.twitter}
+              insta = {speaker.instagram}
+              linkedIn = {speaker.linkedin}
+            />
+          ))}
 
-          <SpeakerCard name="Devinder Maheshwari & Kapil Jindal" position="Founders, Beebom" source="https://www.forbesindia.com/media/digital-stars/Beebom_H.webp" description="Founded by IIT prep buddies Devinder Maheshwari (Co-Founder) and Kapil Jindal (Co-Founder & CEO), Beebom is a leading technology media company that has been making waves in the industry for several years, and is catering to more than 100 million monthly users, combined on all platforms. As of March 2023, Beebom has amassed 2.62 million subscribers and 485 million total views on YouTube. " insta='https://instagram.com/devindermaheshwari?igshid=YmMyMTA2M2Y'/>
-          <SpeakerCard name="Dr. Deepti Pinto Rosario" position="OB/GYN | Female Healthcare Activist" source="https://pintorosariohospital.com/wp-content/uploads/2019/07/1J3A9840-min1-1-700x800.jpg" description="Dr. Deepti is a third generation Obstetrician Gynaecologist from the town of Porvorim, Goa. She's not only a leading figure in the obstetrics and gynaecology community, but is also an activist fighting for gender equity in healthcare. She is a strong believer in empowering women to take charge of their own health. Through her Instagram handle @drwombastic (followed by over 20,000 people), she seeks to spread awareness about everyday gynaecological and pregnancy related issues, including menstrual health." insta='https://instagram.com/drwombastic?igshid=YmMyMTA2M2Y=' />
-          <SpeakerCard name="Praachi Nagpal" position="Miss Grand India 2022 | Model" source="https://missgrandinternational.com/media/2022/09/Profile_31.jpg" description="Praachi Nagpal, the reigning Miss Grand India, has made a lasting impact in the pageant world with her remarkable journey. Her achievements in the competition continue to reverberate throughout the industry. Praachi's commitment to her cause is evident through her numerous speaking engagements and collaborations with mental health organisations. Despite facing the challenges of type 1 diabetes, Praachi has not let it get the better of her. Her story serves as a powerful reminder that no obstacle is insurmountable if you have the strength and courage to face it head-on." />
-          <SpeakerCard name="Chef Pankaj Bhadouria" position="Masterchef | Restaurateur | Speaker | Author |" source="https://www.pankajbhadouria.com/wp-content/themes/pb/images/og-pankajbhadouria.png" description="Chef Pankaj Bhadouria is a renowned chef, author, and television personality in India whose culinary journey continues to reverberate in the hearts of food enthusiasts around the world. She rose to fame as the winner of the popular cooking competition show, MasterChef India, in 2010, where she impressed the judges with her innovative cooking techniques and flavorful dishes." insta='https://instagram.com/masterchefpankajbhadouria?igshid=YmMyMTA2M2Y' />
 
-          <SpeakerCard name="Sudhir R" position="India's First Flute boxer | Asia's Got Talent Finalist" source="https://media.licdn.com/dms/image/C4D03AQGq05XsZardbQ/profile-displayphoto-shrink_800_800/0/1626330698761?e=2147483647&v=beta&t=N850UThHsPDyjpdIfXwRA1S_34qw-eu7Qa7NO5Yvj4s" description="Thinking of a remix, like classical musical instruments such as Flute and hip-hop culture’s beatboxing? Well, Sudhir R has left the entire nation spellbound with this amalgamation. He has achieved various feats like being the semi-finalist in Asia’s Got Talent and India’s Got Talent. His journey, however, has not been without challenges. From practising with a cracked flute to selling his shows worldwide, Sudhir is a motivation to the upcoming generation of musicians and his performances are a true celebration of the power of music to transcend norms." insta='https://instagram.com/sudhirbbxr?igshid=YmMyMTA2M2Y' />
+
+
+
+
 
         </div>
       </div>
